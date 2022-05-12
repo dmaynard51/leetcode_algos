e186a770b690bfbd1849863ba79b09aa1f099833
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-05-12 09:31:13 -0700

    Time: 1953 ms (56.12%), Space: 30.7 MB (78.12%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -5,21 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        res = []
-        
+        r = l = 0
         q = deque()
-        l = 0
-        r = 0
+        res = []
         while r < len(nums):
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
             q.append(r)
+            
             if l > q[0]:
                 q.popleft()
-            if r + 1 >= k:
+            if r+1 >= k:
                 res.append(nums[q[0]])
                 l+= 1
-            r+= 1
+            r+=1
         return res
-            
-                
