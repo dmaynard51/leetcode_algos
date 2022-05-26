38ed72982464cf55988e645862d6a5a9d4e3957a
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-05-26 09:48:04 -0700

    Time: 20 ms (68.96%), Space: 13.6 MB (13.43%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -11,8 +11,11 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if not p and not q:
-            return True
-        if not p or not q or p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        def dfs(l,r):
+            if not l and not r:
+                return True
+            if not l or not r or p.val != q.val:
+                return False
+            return l.val == r.val and dfs(l.left, r.left) and dfs(l.right, r.right)
+        
+        return dfs(p, q)
