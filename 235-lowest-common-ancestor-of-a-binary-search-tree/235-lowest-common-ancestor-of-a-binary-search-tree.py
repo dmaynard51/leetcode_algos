ab779d5e760a56b04e14de0830e33df4cd7de0fc
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-05-29 11:45:45 -0700

    Time: 101 ms (25.18%), Space: 21.5 MB (27.71%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,10 +14,11 @@ class Solution(object):
         :rtype: TreeNode
         """
         
+        
         while root:
-            if p.val < root.val and q.val < root.val:
-                root = root.left
-            elif p.val > root.val and q.val > root.val:
+            if root.val < p.val and root.val < q.val:
                 root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
             else:
                 return root
