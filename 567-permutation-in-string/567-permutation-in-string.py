71a8c894456b7fc6f4fd501e99ca59d90fa85c07
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-05-31 13:34:14 -0700

    Time: 89 ms (52.45%), Space: 14.3 MB (7.17%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         dct1 = collections.Counter(s1)
         counter = 0
-        
+        l = 0
         for r in range(len(s2)):
             if s2[r] in dct1:
                 dct1[s2[r]] -= 1
@@ -15,11 +15,11 @@ class Solution(object):
                     counter += 1
                 
             if r >= len(s1):
-                if s2[r-len(s1)] in dct1:
-                    if dct1[s2[r-len(s1)]] == 0:
+                if s2[l] in dct1:
+                    if dct1[s2[l]] == 0:
                         counter -= 1
-                    dct1[s2[r-len(s1)]] += 1
-  
+                    dct1[s2[l]] += 1
+                l+=1
 
             if counter == len(dct1):
                 return True
