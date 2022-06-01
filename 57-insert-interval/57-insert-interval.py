1b7335ad93b5ad9440b2cd02ca44bc98eab31be6
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-01 13:41:53 -0700

    Time: 64 ms (76.92%), Space: 16.7 MB (94.32%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        intervals.append(newInterval)
+        intervals.sort(key = lambda i: i[0])
+        res = []
+        for i in range(len(intervals)):
+            if len(res) > 0 and intervals[i][0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+        return res
