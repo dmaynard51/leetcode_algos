1c786015ce0bc368de5e821999437701bc1eb4bd
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-02 12:07:07 -0700

    Time: 408 ms (34.85%), Space: 22.2 MB (11.52%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -4,33 +4,29 @@ class Solution(object):
         :type rooms: List[List[int]]
         :rtype: None Do not return anything, modify rooms in-place instead.
         """
-        q = deque()
-        rms = 0
-        rows = len(rooms)
-        columns = len(rooms[0])
         visit = set()
+        
+        q = deque()
+        
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
-                    q.append([i, j, 0])
-                    visit.add((i, j))
-                elif rooms[i][j] == float('inf'):
-                    r += 1
-
+                    q.append([i,j, 0])
+        
+        
         while q:
             lenQ = len(q)
+            
             for i in range(lenQ):
-                #print q
-                r, c, steps = q.popleft()
-                
-                for r1, c1 in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
-                    if r1 < 0 or r1 >= rows or c1 < 0 or c1 >= columns or rooms[r1][c1] == -1 or (r1, c1) in visit:
+                i, j, steps = q.popleft()
+                for r1, c1 in ((i+1, j), (i-1, j), (i,j+1),(i, j-1)):
+                    if r1 < 0 or r1 >= len(rooms) or c1 < 0 or c1 >= len(rooms[0]) or rooms[r1][c1] == -1 or (r1,c1) in visit:
                         continue
-                    q.append([r1, c1, steps + 1])
+                    
+                    q.append([r1,c1, steps+1])
+                    visit.add((r1,c1))
                     rooms[r1][c1] = min(rooms[r1][c1], steps + 1)
-                    visit.add((r1, c1))
+
                 
                 
-                    
-        
-                    
+            
