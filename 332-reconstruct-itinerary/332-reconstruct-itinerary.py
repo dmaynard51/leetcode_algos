0471f98a5e56c752923565af9524f348432240b4
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-06 21:09:50 -0700

    Time: 75 ms (67.48%), Space: 14.7 MB (8.39%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        adj = {src:[] for src, dst in tickets}
+        tickets.sort()
+        for src, dst in tickets:
+            adj[src].append(dst)
+        
+        res = ['JFK']
+        
+        def dfs(src):
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
+            
+            temp = list(adj[src])
+            
+            for i, v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+                if dfs(v):
+                    return True
+                adj[src].insert(i, v)
+                res.pop()
+            return False
+        dfs('JFK')
+        return res
