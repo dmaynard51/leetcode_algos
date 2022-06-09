2a144435d97c930011b1532fae0606f3b903d4b4
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-09 12:26:39 -0700

    Time: 391 ms (88.70%), Space: 15.6 MB (86.92%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -6,27 +6,27 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        #djiktras
+        
         adj = defaultdict(list)
         
-        for u, v, w in times:
-            adj[u].append([w, v])
-        visit= set()
-        t = 0
-        #print adj
+        visit = set()
+        
+        for src, dst, cost in times:
+            adj[src].append([cost, dst])
         q = [[0, k]]
-        #print adj
+        res = 0
+
         while q:
-            w1, v1 = heapq.heappop(q)
-            
-            if v1 in visit:
+            c1, src1 = heapq.heappop(q)
+            if src1 in visit:
                 continue
-            visit.add(v1)
-            t = max(t, w1)
-            for w2, v2 in adj[v1]:
-                if v2 in visit:
+            visit.add(src1)
+            res = max(res, c1)
+            for c2, src2 in adj[src1]:
+                if src2 in visit:
                     continue
-                heapq.heappush(q, [w2 + w1, v2])
+                heapq.heappush(q, [c2 + c1, src2])
         if len(visit) == n:
-            return t
+            return res
         return -1
+        
