2187f37e142818601590de2d94ca7d0f6f907820
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-15 10:44:12 -0700

    Time: 5566 ms (16.21%), Space: 139.8 MB (23.71%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -4,34 +4,20 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
-        adj = {i:[] for i in range(len(points))}
-        
-        for i in range(len(points)):
-            x1, y1 = points[i]
-            for j in range(i+1, len(points)):
-                x2, y2 = points[j]
-                dist = abs(x1-x2) + abs(y1-y2)
-                adj[i].append([dist,j])
-                adj[j].append([dist, i])
-        visit = set()
-        res = 0
-                              
-        q = [[0,0]]
-        
-        while q:
-            cost, src = heapq.heappop(q)
-            if src in visit:
-                continue
-            res += cost
-            visit.add(src)
-            
-            for cost2, src2 in adj[src]:
-                if src2 in visit:
-                    continue
-                heapq.heappush(q, [cost2, src2])
-        return res
-            
-                             
-                              
-                              
-                    
+        manhattan = lambda p1, p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        n, c = len(points), collections.defaultdict(list)
+        for i in range(n):
+            for j in range(i+1, n):
+                d = manhattan(points[i], points[j])
+                c[i].append((d, j))
+                c[j].append((d, i))
+        cnt, ans, visited, heap = 1, 0, [0] * n, c[0]
+        visited[0] = 1
+        heapq.heapify(heap)
+        while heap:
+            d, j = heapq.heappop(heap)
+            if not visited[j]:
+                visited[j], cnt, ans = 1, cnt+1, ans+d
+                for record in c[j]: heapq.heappush(heap, record)
+            if cnt >= n: break        
+        return ans
