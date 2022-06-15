5ee175eb20aee03af27f3e5760cacfcede541bed
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-14 22:40:12 -0700

    Time: 124 ms (67.20%), Space: 14.2 MB (54.84%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.py
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -4,21 +4,21 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visit = set((0, 0))
         q = [[grid[0][0], 0, 0]]
         
-        visit = set()
-        visit.add((0,0))
-        n = len(grid)
-        directions = [[1, 0], [-1, 0], [0,1], [0, -1]]
+        
+        
         while q:
-            t, r, c = heapq.heappop(q)
-            if r == n-1 and c == n-1:
-                return t            
+            cost1, i, j = heapq.heappop(q)
+            if i == len(grid)-1 and j == len(grid[0])-1:
+                return cost1
             
-            for r1, c1 in directions:
-                r1, c1 = r1 + r, c1 + c
-
-                if r1 < 0 or r1 >= n or c1 < 0 or c1 >= n or (r1, c1) in visit:
+            for i2, j2 in dirs:
+                i2, j2 = i2 + i, j2 + j
+                
+                if i2 < 0 or i2 >= len(grid) or j2 < 0 or j2 >= len(grid[0]) or (i2, j2) in visit:
                     continue
-                visit.add((r1, c1))
-                heapq.heappush(q, [max(t, grid[r1][c1]), r1, c1])
+                visit.add((i2, j2))
+                heapq.heappush(q, [max(cost1, grid[i2][j2]), i2, j2])
