9f11e864fc3570236e677b4f00cbf7636b2ea865
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-15 12:19:31 -0700

    Time: 3080 ms (36.91%), Space: 143.4 MB (21.87%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -4,20 +4,25 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
-        manhattan = lambda p1, p2: abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
         n, c = len(points), collections.defaultdict(list)
         for i in range(n):
+            x1, y1 = points[i]
             for j in range(i+1, n):
-                d = manhattan(points[i], points[j])
+                x2, y2 = points[j]
+                d = abs(x1-x2) + abs(y1-y2)
                 c[i].append((d, j))
                 c[j].append((d, i))
         cnt, ans, visited, heap = 1, 0, [0] * n, c[0]
         visited[0] = 1
         heapq.heapify(heap)
+        visit = set()
+        visit.add(0)
         while heap:
             d, j = heapq.heappop(heap)
-            if not visited[j]:
-                visited[j], cnt, ans = 1, cnt+1, ans+d
+            if j not in visit:
+                cnt, ans = cnt+1, ans+d
+                visit.add(j)
                 for record in c[j]: heapq.heappush(heap, record)
             if cnt >= n: break        
         return ans
