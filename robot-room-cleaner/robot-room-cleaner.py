d02fe728b5371d0183b714ec01fa53f6f6530cee
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-18 21:39:02 -0700

    Time: 66 ms, Memory: 14.5 MB - LeetHub

---
+++ robot-room-cleaner/robot-room-cleaner.py
@@ -0,0 +1,59 @@
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot(object):
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution(object):
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        self.dfs(robot, 0, 0, 0, 1, set())
+    def dfs(self, robot, x, y, direction_x, direction_y, visit):
+        robot.clean()
+        visit.add((x, y))
+        
+        for i in range(4):
+            neighbor_x = x + direction_x
+            neighbor_y = y + direction_y
+            if (neighbor_x, neighbor_y) not in visit and robot.move():
+                self.dfs(robot, neighbor_x, neighbor_y, direction_x, direction_y, visit)
+                robot.turnLeft()
+                robot.turnLeft()
+                robot.move()
+                robot.turnLeft()
+                robot.turnLeft()
+
+            robot.turnLeft()
+            direction_x, direction_y = -direction_y, direction_x
+ 
+                
+        
