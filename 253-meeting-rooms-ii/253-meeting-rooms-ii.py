0af8accc7da72b4712f506c55592b49782f58e3d
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-18 21:52:49 -0700

    Time: 83 ms (54.14%), Space: 16.9 MB (85.04%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        start = sorted([i[0] for i in intervals])
-        end  = sorted([i[1] for i in intervals])
         
-        s, e, res = 0, 0, 0
+        start = sorted(i[0] for i in intervals)
+        end = sorted(i[1] for i in intervals)
+        
+        s, e = 0, 0
+        res = 0
         count = 0
         while s < len(intervals):
             if start[s] < end[e]:
                 count += 1
                 s += 1
             else:
-                count -= 1
                 e += 1
+                count -=1
             res = max(res, count)
         return res
