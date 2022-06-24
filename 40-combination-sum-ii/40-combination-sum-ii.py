6399646e8cb3e9d998e65247f79b6fea83573a33
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-23 20:44:58 -0700

    Time: 85 ms (50.69%), Space: 13.4 MB (64.58%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -6,22 +6,19 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
+        
         res = []
         candidates.sort()
-        #print candidates
         def dfs(idx, arr, sm, path):
             if sm == target:
                 res.append(path)
                 return
-            if idx >= len(candidates) or sm > target:
+            if idx >= len(arr) or sm > target:
                 return
-            #print path
+            
             for i in range(idx, len(arr)):
                 if i > idx and arr[i] == arr[i-1]:
                     continue
-                #print path
                 dfs(i+1, arr, sm + arr[i], path + [arr[i]])
         dfs(0, candidates, 0, [])
         return res
-                
-                
