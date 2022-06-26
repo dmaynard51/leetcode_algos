e706e79e88608a04065d6b18ffaa832eda83dd48
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-06-25 20:25:18 -0700

    Time: 826 ms (46.76%), Space: 32.8 MB (25.86%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -4,26 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-
-    
-        res = []
+        def check(l, r, s):
+            while l < r:
+                if s[l] != s[r]:
+                    
+                    return False
+                l+= 1
+                r -= 1
+            return True
+        
         part = []
+        res = []
         def dfs(i):
             if i >= len(s):
                 res.append(part[:])
                 return
             for j in range(i, len(s)):
-                if self.check(i, j, s):
+                if check(i, j, s):
                     part.append(s[i:j+1])
-                    dfs(j+1)
+                    dfs(j + 1)
                     part.pop()
         dfs(0)
         return res
-    
-    def check(self, l,r, s):
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            l+=1
-            r -=1
-        return True    
+                    
