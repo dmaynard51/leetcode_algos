e145903df41a7d31413de1f6d50868c902ea8461
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-05 10:37:38 -0700

    Time: 21 ms (68.66%), Space: 13.1 MB (96.73%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
-        
-        j1 = 1
-        j2 = 2
+        step1 = 1
+        step2 = 2
         
         if n <= 2:
             return n
-        # 1, 2, 3, 4, 5, 6
-        # 1, 2, 3, 5, 8, 13
-        
         for i in range(2, n):
-            temp = (j1 + j2)
-            j1 = j2
-            j2 = temp
-        return j2
+            temp = step1 + step2
+            step1 = step2
+            step2 = temp
+        return step2
