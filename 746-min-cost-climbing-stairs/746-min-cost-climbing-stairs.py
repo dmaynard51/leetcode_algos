793dafea1fef0a4700d2e30d858bf1d73b83082b
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-09 20:42:28 -0700

    Time: 91 ms (11.28%), Space: 13.3 MB (93.76%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -5,11 +5,11 @@ class Solution(object):
         :rtype: int
         """
         dp = [0 for i in range(len(cost)+1)]
+        
         dp[0] = cost[0]
         dp[1] = cost[1]
-        
         if len(cost) <= 2:
-            return min(cost[-1], cost[-2])
+            return min(cost[0], cost[1])
         
         for i in range(2, len(cost)):
             dp[i] = min(cost[i] + dp[i-1], cost[i] + dp[i-2])
