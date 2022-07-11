dcc6f40e3e23075a15b64fd47a565408592e145b
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-11 13:05:00 -0700

    Time: 83 ms (87.73%), Space: 16.7 MB (16.46%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -5,34 +5,35 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
-        
-        visit = set()
-        cycle = set()
-        
-        adj = {i:[] for i in range(numCourses)}
+        adj = defaultdict(list)
         
         for src, dst in prerequisites:
             adj[src].append(dst)
-        
+        visit = set()
+        cycle = set()
         res = []
-        
         def dfs(node):
             if node in cycle:
                 return False
             if node in visit:
                 return True
+            
             cycle.add(node)
             
             for nei in adj[node]:
                 if not dfs(nei):
                     return False
-            cycle.remove(node)
+            
             visit.add(node)
+            cycle.remove(node)
             res.append(node)
             return True
         
         for i in range(numCourses):
             if not dfs(i):
                 return []
-        return res
         
+        if len(visit) == numCourses:
+            return res
+        else:
+            return []
