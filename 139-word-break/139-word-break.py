86e164fb66c1b97d21f3ea3231c552f657abb93f
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-12 11:12:21 -0700

    Time: 47 ms (35.60%), Space: 13.4 MB (89.93%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -10,12 +10,15 @@ class Solution(object):
         
         dp[0] = 1
         #leetcode
+        #1000
         #012345678
-        #1000        
+        #1000  
         for i in range(1, len(dp)):
             for w in wordDict:
                 n = len(w)
-                if i >= n - 1 and w == s[i - n:i] and dp[i-n]:
+                #print s[i-n:i]
+                if i >= n  and w == s[i - n:i] and dp[i-n]:
+                    
                     dp[i] = max(dp[i], dp[i-n] + 1)
         #print dp
         return dp[-1]
