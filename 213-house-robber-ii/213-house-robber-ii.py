fc0c475a8789ee8972e10dac6b0d2f5bbb6bc51b
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-12 15:09:06 -0700

    Time: 14 ms (96.88%), Space: 13.6 MB (13.81%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def dfs(n):
+        
+        def check(arr):
             rob1, rob2 = 0, 0
             
-            for i in n:
-                temp = max(rob1 + i, rob2)
+            for i in arr:
+                temp = max(rob2, rob1 + i)
                 rob1 = rob2
                 rob2 = temp
             return rob2
-        if len(nums) <= 1:
+        if len(nums) <=1 :
             return nums[-1]
-        return max(dfs(nums[1:]), dfs(nums[:-1]))
+        return max(check(nums[1:]), check(nums[:-1]) )
+        
