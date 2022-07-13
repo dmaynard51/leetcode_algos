a061e0c69d99afb702cef47fc8808768c9612e6f
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-12 18:44:27 -0700

    Time: 796 ms (45.42%), Space: 28 MB (26.37%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        pos = [0 for i in range(len(nums))]
-        neg = [0 for i in range(len(nums))]
+        n = len(nums)
+        
+        pos = [0 for i in range(n)]
+        neg = [0 for i in range(n)]
+        
         pos[0] = 1 if nums[0] > 0 else 0
         neg[0] = 1 if nums[0] < 0 else 0
         
         for i in range(1, len(nums)):
-            if nums[i] < 0:
-                pos[i] = 1 + neg[i - 1] if neg[i - 1] else 0
-                neg[i] = 1 + pos[i - 1] if pos[i - 1] else 1
-            elif nums[i] > 0:
-                pos[i] = 1 + pos[i - 1] if pos[i - 1] else 1
-                neg[i] = 1 + neg[i - 1] if neg[i - 1] else 0
+            if nums[i] > 0:
+                pos[i] = pos[i-1] + 1 if pos[i-1] else 1
+                neg[i] = neg[i-1] + 1 if neg[i-1] else 0
+            elif nums[i] < 0:
+                pos[i] = neg[i-1] + 1 if neg[i-1] else 0
+                neg[i] = pos[i-1] + 1 if pos[i-1] else 1
         return max(pos)
