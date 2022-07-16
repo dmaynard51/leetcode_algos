ac005ac104f7f67727938e295646f4fc339c6422
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-16 14:05:10 -0700

    Time: 797 ms (31.01%), Space: 16.7 MB (79.63%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -7,14 +7,16 @@ class Solution(object):
         def check(w, wordDict):
             if not wordDict:
                 return False
-            dp = [0 for i in range(len(w)+1)]
+            
+            dp = [0 for i in range(len(w) + 1)]
             dp[0] = 1
             
             for i in range(len(dp)):
                 for j in range(i):
                     if dp[j] and w[j:i] in wordDict:
-                        dp[i] = max(dp[i], dp[j] + 1)
+                        dp[i] = max(dp[i], dp[j]+1)
                         break
+            #print dp
             return dp[-1]
         
         words.sort(key = len)
