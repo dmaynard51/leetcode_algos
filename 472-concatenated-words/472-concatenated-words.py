3c3fb09dd18399f37567be42439ea564d66d2553
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-16 13:49:45 -0700

    Time: 662 ms (41.66%), Space: 16.7 MB (86.11%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        def check(w, wordDict):
+            if not wordDict:
+                return False
+            dp = [0 for i in range(len(w)+1)]
+            dp[0] = 1
+            
+            for i in range(len(dp)):
+                for j in range(i):
+                    if dp[j] and w[j:i] in wordDict:
+                        dp[i] = max(dp[i], dp[j] + 1)
+                        break
+            return dp[-1]
+        
+        words.sort(key = len)
+        pre = set()
+        res = []
+        #print words
+        for w in words:
+            if check(w, pre):
+                res.append(w)
+            pre.add(w)
+        return res
