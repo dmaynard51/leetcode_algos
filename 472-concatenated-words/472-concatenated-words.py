67b9523e5cd5fe0ea934d5597753472f084771f8
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-17 09:50:06 -0700

    Time: 1089 ms (17.59%), Space: 16.5 MB (95.37%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -4,27 +4,25 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        def check(w, wordDict):
+        def check(s, wordDict):
             if not wordDict:
                 return False
             
-            dp = [0 for i in range(len(w) + 1)]
+            dp = [0 for i in range(len(s)+1)]
             dp[0] = 1
-            
             for i in range(len(dp)):
                 for j in range(i):
-                    if dp[j] and w[j:i] in wordDict:
-                        dp[i] = max(dp[i], dp[j]+1)
-                        break
-            #print dp
+                    if dp[j] and s[j:i] in wordDict:
+                        dp[i] = max(dp[i], dp[j] + 1)
+            
             return dp[-1]
         
-        words.sort(key = len)
         pre = set()
         res = []
-        #print words
-        for w in words:
-            if check(w, pre):
-                res.append(w)
-            pre.add(w)
+        words.sort(key = len)
+        for word in words:
+            if check(word, pre):
+                res.append(word)
+            pre.add(word)
         return res
+             
