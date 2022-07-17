9b5e20bb32e4f03d057682edb0feba4a8a0e69b0
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-17 09:27:21 -0700

    Time: 229 ms (62.11%), Space: 14.7 MB (82.81%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        n = len(s)
-        cnt0 = s.count('0')
-        cnt1 = 0
-        res = n - cnt0
+        
+        count0 = s.count('0')
+        count1 = 0
+        res = len(s) - count0
+        
         for i in s:
             if i == '0':
-                cnt0 -= 1
+                count0 -=1
             elif i == '1':
-                res = min(res, cnt0 + cnt1)
-                cnt1 +=1
+                res = min(res, count0 + count1)
+                count1 += 1
         return res
