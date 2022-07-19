14e6204517a817001120d712dd2cacc85a27caed
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-19 09:29:43 -0700

    Time: 47 ms (82.74%), Space: 13.9 MB (92.16%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -7,13 +7,10 @@ class Solution(object):
         :rtype: List[str]
         """
         users = defaultdict(list)
-        
         for user, time, web in sorted(zip(username, timestamp, website)):
             users[user].append(web)
-            
         counter = collections.Counter()
-        
         for user, website in users.items():
             counter.update(Counter(set(combinations(website, 3))))
-        
+            
         return max(sorted(counter), key = counter.get)
