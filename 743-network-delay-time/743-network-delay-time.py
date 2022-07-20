206d65cdb27332a6e52d96ad7263181db8d3cb0d
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-20 16:59:55 -0700

    Time: 379 ms (95.28%), Space: 15.5 MB (92.82%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -6,29 +6,31 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
-        
         adj = defaultdict(list)
+        visit = set()
+        
+        res = 0
         
         for u, v, w in times:
             adj[u].append([w, v])
-            
-        visit = set()
-        q = [[0, k]]
-        res = 0
-        while q:
-            w1, src1 = heapq.heappop(q)
-            if src1 in visit:
-                continue
-            visit.add(src1)
-            res = max(w1, res)
-            
-            for w2, src2 in adj[src1]:
-                if src2 in visit:
-                    continue
-                heapq.heappush(q, [w2 + w1, src2])
         
+        q = [[0, k]]
+        
+        while q:
+            w1, v1 = heapq.heappop(q)
+            
+
+            
+            if v1 in visit:
+                continue
+            visit.add(v1)
+            res = max(res, w1)
+            
+            for w2, v2 in adj[v1]:
+                if v2 in visit:
+                    continue
+                heapq.heappush(q, [w2+ w1, v2])
+            
         if len(visit) == n:
             return res
         return -1
-            
