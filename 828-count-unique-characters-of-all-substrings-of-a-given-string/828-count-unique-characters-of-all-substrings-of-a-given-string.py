e4afaaf83c26c9035bbc1874637dd7f5139ba6ef
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-22 20:52:54 -0700

    Time: 456 ms (50.96%), Space: 19 MB (23.08%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -1,14 +1,15 @@
 class Solution(object):
-    def uniqueLetterString(self, S):
+    def uniqueLetterString(self, s):
         """
         :type s: str
         :rtype: int
         """
-        res=[0]*(len(S)+1)
-        idxs=[[-1,-1]]*26
-        for i,c in enumerate(S):
-            code=ord(c)-ord('A')
-            first,second=idxs[code]
-            res[i+1]=1+res[i]+(i-1-second)-(second-first)
-            idxs[code]=[second,i]
-        return sum(res)%(10**9+7)
+        idx = [[-1, -1] for i in range(26)]
+        res = [0 for i in range(len(s)+1)]
+        
+        for i, c in enumerate(s):
+            code = ord(c) - ord('A')
+            first, second = idx[code]
+            res[i+1] = 1 + res[i] + (i - 1 - second) - (second - first)
+            idx[code] = [second, i]
+        return sum(res) % (10**9+7)
