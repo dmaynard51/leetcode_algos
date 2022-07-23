b6ea316f420833353f065a18758423a32f951a14
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-23 08:40:59 -0700

    Time: 395 ms (62.50%), Space: 19.1 MB (20.67%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        idx = [[-1, -1] for i in range(26)]
+        idx = [[-1, -1] for i in range(26)] 
         res = [0 for i in range(len(s)+1)]
         
-        for i, c in enumerate(s):
+        for i,c in enumerate(s):
             code = ord(c) - ord('A')
             first, second = idx[code]
-            res[i+1] = 1 + res[i] + (i - 1 - second) - (second - first)
+            res[i+1] = res[i] + 1 + (i - 1 - second) - (second - first)
             idx[code] = [second, i]
         return sum(res) % (2**32-1)
