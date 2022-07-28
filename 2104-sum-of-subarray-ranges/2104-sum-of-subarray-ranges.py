6b0450a7c400fdcb1648291d8a04bd3917e52d9b
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-07-28 08:15:59 -0700

    Time: 3834 ms (18.90%), Space: 13.6 MB (75.58%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -5,11 +5,10 @@ class Solution(object):
         :rtype: int
         """
         res = 0
-
         for i in range(len(nums)):
-            mn,mx = nums[i], nums[i]
+            mn= mx = nums[i]
             for j in range(i, len(nums)):
                 mn = min(mn, nums[j])
                 mx = max(mx, nums[j])
-                res += (mx -mn)
+                res += (mx - mn)
         return res
