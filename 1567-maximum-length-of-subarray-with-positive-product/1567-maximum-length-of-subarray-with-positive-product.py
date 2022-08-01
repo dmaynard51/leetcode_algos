7b8b48cc9d74063e42d903abe7177308765b8f41
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-01 07:17:29 -0700

    Time: 857 ms (41.61%), Space: 28 MB (29.03%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        
-        pos = [0 for i in range(n)]
-        neg = [0 for i in range(n)]
+        pos = [0 for i in range(len(nums))]
+        neg = [0 for i in range(len(nums))]
         
         pos[0] = 1 if nums[0] > 0 else 0
         neg[0] = 1 if nums[0] < 0 else 0
@@ -19,4 +17,5 @@ class Solution(object):
             elif nums[i] < 0:
                 pos[i] = neg[i-1] + 1 if neg[i-1] else 0
                 neg[i] = pos[i-1] + 1 if pos[i-1] else 1
+        
         return max(pos)
