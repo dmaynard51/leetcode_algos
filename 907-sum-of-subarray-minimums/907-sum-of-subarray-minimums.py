fd2f2b126316168e7d7487212868a641c056a979
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-01 11:26:08 -0700

    Time: 715 ms (33.07%), Space: 17.6 MB (29.53%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -4,15 +4,20 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
+        
         A = [0] + arr
-        res = [0] * len(A)
+        
+        dp = [0 for i in range(len(A))]
+        
         stack = [0]
-        #print A
+        
         for i in range(len(A)):
             while A[stack[-1]] > A[i]:
                 stack.pop()
             j = stack[-1]
-            res[i] = res[j] + (i-j)*A[i]
+            dp[i] = dp[j] + (i-j)*A[i]
             stack.append(i)
-        return sum(res) % (10**9+7)
-            
+        #print dp
+        return sum(dp) % (10**9+7)
+                
+                
