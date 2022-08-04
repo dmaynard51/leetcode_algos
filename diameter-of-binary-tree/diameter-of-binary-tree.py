186724035337a4ead8a52f1cd07d9b900b1844fb
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-04 16:23:14 -0700

    Time: 47 ms, Memory: 16.1 MB - LeetHub

---
+++ diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = [0]
+        
+        def dfs(node):
+            if not node:
+                return -1
+            l, r = dfs(node.left), dfs(node.right)
+            res[0] = max(res[0], l + r + 2)
+            return 1 + max(l, r)
+        dfs(root)
+        return res[0]
