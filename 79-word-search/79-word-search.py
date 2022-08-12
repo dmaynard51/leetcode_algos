25f6ab1c3da993375211d636b9a4e2a8267bed5d
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-12 12:08:49 -0700

    Time: 8096 ms (35.78%), Space: 13.6 MB (47.31%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -6,24 +6,20 @@ class Solution(object):
         :rtype: bool
         """
         visit = set()
-        self.res = False
-        dirs = [[0,1], [0,-1], [1,0], [-1, 0]]
-        def dfs(i, j, cur):
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[cur] or (i,j) in visit:
+        
+        def dfs(i, j, idx):
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or (i, j) in visit or  word[idx] != board[i][j] or idx >= len(word):
                 return False
-            if cur+1 == len(word) and board[i][j] == word[cur]:
+            if idx ==  len(word)-1 and board[i][j] == word[idx]:
                 return True
-            
-            
-            visit.add((i,j))
-            res = dfs(i+1,j, cur + 1) or dfs(i-1, j, cur + 1) or dfs(i, j+1, cur + 1) or dfs(i, j-1, cur + 1)
+            visit.add((i, j))
+            res = dfs(i+1, j, idx + 1) or dfs(i-1, j, idx + 1) or dfs(i, j+1, idx + 1) or dfs(i, j-1, idx + 1)
             
             if res:
                 return True
-            visit.remove((i,j))
-                
+            visit.remove((i, j))
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(i,j, 0):
+                if dfs(i, j, 0):
                     return True
         return False
