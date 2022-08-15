4d9628753ac3583f05bb53693dc28f9d163a0179
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-15 08:39:01 -0700

    Time: 195 ms (40.01%), Space: 16.4 MB (24.89%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        
         visit = set()
         def dfs(i,j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i,j) in visit or grid[i][j] == 0:
                 return 0
-            visit.add((i,j))
-            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
-        
+            visit.add((i,j)) 
+            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j -1)
         res = 0
-        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                res = max(res, dfs(i, j))
+                res = max(res, dfs(i,j))
         return res
