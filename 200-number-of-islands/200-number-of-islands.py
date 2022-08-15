828c2dc00fb5db319ecdfb51fe5f20543f37f986
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-15 08:27:42 -0700

    Time: 523 ms (31.60%), Space: 35.5 MB (5.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -5,21 +5,18 @@ class Solution(object):
         :rtype: int
         """
         visit = set()
-        
         def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i, j) in visit or grid[i][j] != '1':
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i,j) in visit or grid[i][j] != '1':
                 return 0
             visit.add((i,j))
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-            #visit.remove((i, j))
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            #visit.remove((i,j))
             return 1
-        
         res = 0
-        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-               res += dfs(i, j)
+                res += dfs(i,j)
         return res
