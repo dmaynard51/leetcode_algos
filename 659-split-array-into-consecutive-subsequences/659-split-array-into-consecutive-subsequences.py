f044f392b2c70d2d5a4195928af5552e4dcc22e8
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-19 09:05:40 -0700

    Time: 1008 ms (26.92%), Space: 14.9 MB (48.08%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPossible(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        counter = collections.Counter(nums)
+        seq = defaultdict(int)
+        
+        for num in nums:
+            if counter[num] == 0:
+                continue
+            elif seq[num] > 0:
+                seq[num] -=1
+                seq[num+1] += 1
+            elif counter[num+1] > 0 and counter[num+2] > 0:
+                #counter[num] -=1
+                counter[num+1] -=1
+                counter[num+2] -= 1
+                seq[num+3] += 1
+            else:
+                return False
+            counter[num] -= 1
+        return True
