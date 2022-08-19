4951fb6e33fc1ceeb4ab41ca9a8dff25e77ba14d
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-19 09:09:14 -0700

    Time: 879 ms (36.54%), Space: 15.1 MB (7.69%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -11,14 +11,14 @@ class Solution(object):
             if counter[num] == 0:
                 continue
             elif seq[num] > 0:
-                seq[num] -=1
+                seq[num] -= 1
                 seq[num+1] += 1
-            elif counter[num+1] > 0 and counter[num+2] > 0:
-                #counter[num] -=1
-                counter[num+1] -=1
-                counter[num+2] -= 1
-                seq[num+3] += 1
+            elif counter[num + 1] > 0 and counter[num + 2] > 0:
+                counter[num + 1] -= 1
+                counter[num + 2] -=1
+                seq[num + 3] += 1
             else:
                 return False
             counter[num] -= 1
         return True
+        
