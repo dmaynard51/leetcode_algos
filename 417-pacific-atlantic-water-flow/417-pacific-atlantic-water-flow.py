7c265bd5f5c77ab37a4296e5bf02f8339441ae7d
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-26 08:02:24 -0700

    Time: 540 ms (19.44%), Space: 15.3 MB (53.80%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -7,35 +7,29 @@ class Solution(object):
         pac = set()
         atl = set()
         
-        res = []
         rows = len(heights)
-        columns = len(heights[0])
+        cols = len(heights[0])
         
-        def dfs(i, j, st, prev):
-            if i < 0 or i >= rows or j < 0 or j >= columns or heights[i][j] < prev or (i,j) in st:
+        def dfs(i, j, visit, prev):
+            if i < 0 or i >= rows or j < 0 or j >= cols or (i, j) in visit or heights[i][j] < prev:
                 return
-            
-            st.add((i,j))
-            dfs(i+1, j, st, heights[i][j])
-            dfs(i-1, j, st, heights[i][j])
-            dfs(i, j+1, st, heights[i][j])
-            dfs(i, j-1, st, heights[i][j])
-            
-            
+            visit.add((i,j))
+            dfs(i+1, j, visit, heights[i][j])
+            dfs(i-1, j, visit, heights[i][j])
+            dfs(i, j+1, visit, heights[i][j])
+            dfs(i, j-1, visit, heights[i][j])
         
+        for j in range(rows):
+            dfs(j, 0, pac, 0)
+            dfs(j, cols-1, atl, 0)
         
-        for c in range(columns):
-            dfs(0, c, pac, heights[0][c])
-            dfs(rows-1,c, atl, heights[rows-1][c])
-            
-            
-        for r in range(rows):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, columns-1, atl, heights[r][columns-1] )
-            
-        #print atl, pac
+        for i in range(cols):
+            dfs(0, i, pac, 0)
+            dfs(rows-1, i, atl, 0)
+        
+        res = []
         for i in range(rows):
-            for j in range(columns):
-                if (i,j) in pac and (i,j) in atl:
+            for j in range(cols):
+                if (i,j) in atl and (i,j) in pac:
                     res.append([i,j])
         return res
