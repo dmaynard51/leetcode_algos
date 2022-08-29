67b25cd3a5a153eb368e63e9b879d83f2ff6d03b
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-08-28 18:55:06 -0700

    Time: 56 ms (81.59%), Space: 14.3 MB (30.68%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,12 +5,13 @@ class Solution(object):
         :rtype: int
         """
         l = 0
+        #r = len(s)-1
         res = 0
-        visit = set()
+        contains = set()
         for r in range(len(s)):
-            while s[r] in visit:
-                visit.remove(s[l])
+            while s[r] in contains and l < len(s):
+                contains.remove(s[l])
                 l += 1
-            visit.add(s[r])
-            res = max(res, len(visit))
+            contains.add(s[r])
+            res = max(res, r - l + 1)
         return res
