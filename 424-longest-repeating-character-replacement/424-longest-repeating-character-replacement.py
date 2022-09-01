f84ad81cdd4d2c352d332341cafa7cb31092bcf8
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-09-01 09:07:17 -0700

    Time: 212 ms (26.16%), Space: 14.1 MB (10.20%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -10,12 +10,13 @@ class Solution(object):
         l = 0
         mx = 0
         res = 0
+        
         for r in range(len(s)):
             dct[s[r]] += 1
             mx = max(mx, dct[s[r]])
             
-            if r - l - mx + 1 > k:
+            if r - l + 1 - mx > k:
                 dct[s[l]] -= 1
                 l += 1
-            res = max(res, r - l + 1)
+            res = max(res, r-l+1)
         return res
