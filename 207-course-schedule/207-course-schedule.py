8328ecbe56dae9da456e16629fafaa41af48da2c
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-09-07 20:51:35 -0700

    Time: 187 ms (13.82%), Space: 16.4 MB (46.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -6,29 +6,37 @@ class Solution(object):
         :rtype: bool
         """
         adj = defaultdict(list)
-        visit = set()
-        cycle = set()
         
         for src, nei in prerequisites:
             adj[src].append(nei)
-            
         
+        #print adj
+        visit = set()
+        cycle = set()
         def dfs(node):
+            
             if node in cycle:
                 return False
             if node in visit:
                 return True
-            
+                
             cycle.add(node)
             
             for nei in adj[node]:
                 if not dfs(nei):
                     return False
+                
             cycle.remove(node)
             visit.add(node)
             return True
+            
         
         for i in range(numCourses):
             if not dfs(i):
                 return False
+            
         return True
+        
+        
+        
+        #1 - > 0 -> F
