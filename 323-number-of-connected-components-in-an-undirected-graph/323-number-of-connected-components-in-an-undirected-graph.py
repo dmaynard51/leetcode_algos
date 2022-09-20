357727e1499dc3142f076c8c9ff55462530a50df
Daniel Maynard <Daniel.maynard83@gmail.com>
2022-09-19 19:54:41 -0700

    Time: 74 ms (97.44%), Space: 17 MB (17.37%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -5,16 +5,18 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        res = 0
         adj = defaultdict(list)
-        visit = set()
+        
         for src, nei in edges:
             adj[src].append(nei)
             adj[nei].append(src)
         
+        
+        visit = set()
         def dfs(node):
             if node in visit:
                 return 0
+            
             visit.add(node)
             
             for nei in adj[node]:
@@ -22,6 +24,7 @@ class Solution(object):
             
             return 1
         
+        res = 0
         for i in range(n):
             res += dfs(i)
         return res
