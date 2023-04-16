8b006962ea15a3ee9fd50f4a1af6e0d096e3835e
Daniel Maynard <Daniel.maynard83@gmail.com>
2023-04-15 23:17:00 -0700

    Time: 27 ms (58.80%), Space: 13.9 MB (14.44%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -5,23 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        r = len(nums)-1
         l = 0
-        r= len(nums)-1
         
         while l <= r:
             m = l + (r-l)//2
+            
             if nums[m] == target:
                 return m
             elif nums[l] <= nums[m]:
                 if nums[l] <= target < nums[m]:
-                    r = m - 1
+                    r = m -1
                 else:
                     l = m + 1
             else:
                 if nums[m] <= target <= nums[r]:
                     l = m + 1
                 else:
-                    r = m -1
+                    r = m- 1
                     
-        
         return -1
