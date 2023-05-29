4893501474e39f489b3dd610a3f2e52958e8e893
Daniel Maynard <Daniel.maynard83@gmail.com>
2023-05-28 21:27:00 -0700

    Time: 23 ms (58.06%), Space: 13.3 MB (98.92%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type str2: str
         :rtype: str
         """
-        
-        if str1 == str2:
-            return str1
-        elif len(str1) < len(str2):
+
+        if len(str1) < len(str2):
             return self.gcdOfStrings(str2, str1)
-        elif not str1.startswith(str2):
+        elif not str1.startswith(str2):  # If str1 does not start with str2
             return ''
+        elif len(str2) == 0:  # str2 is empty
+            return str1
         else:
             return self.gcdOfStrings(str1[len(str2):], str2)
         
